d26ba61eaf00217efbe1e715b86a52500f1ad923
Xiaofan Lin <xiaofanlinus@gmail.com>
2022-07-02 20:38:16 -0700

    wip

--- src/Grid.ts
+++ src/Grid.ts
@@ -1,14 +1,16 @@
 let init_grid = (num_segs: number) => {
-    let inidices = [];
+    let indices = [];
+    let tri_indices = [];
     let positions = [];
+    let texcoords = [];
 
-    if(num_segs <= 0) {
+    if (num_segs <= 0) {
         throw new Error('Number of segments should be larger than 0');
     }
     let dh = 2 / num_segs;
 
     // vertices generation
-    for(let a = 0; a <= num_segs; a++) {
+    for (let a = 0; a <= num_segs; a++) {
         for (let b = 0; b <= num_segs; b++) {
             let a_ = -1 + a * dh;
             let b_ = -1 + b * dh;
@@ -20,25 +22,40 @@ let init_grid = (num_segs: number) => {
         }
     }
 
+    dh = 1 / num_segs;
+    // texcorrds generation
+    for (let a = 0; a <= num_segs; a++) {
+        for (let b = 0; b <= num_segs; b++) {
+            let a_ = 0 + a * dh;
+            let b_ = 0 + b * dh;
+
+            texcoords.push(a_);
+            texcoords.push(b_);
+
+        }
+    }
+
     // indices generation
     for (let i = 0; i <= num_segs - 1; i++) {
         for (let j = 0; j <= num_segs - 1; j++) {
             let idx0 = i * (num_segs + 1) + j;
             let idx1 = idx0 + 1;
             let idx2 = (i + 1) * (num_segs + 1) + j;
+            indices.push(idx0, idx1, idx0, idx2);
 
-            inidices.push(idx0, idx1, idx0, idx2);
+            let idx3 = (i + 1) * (num_segs + 1) + (j + 1);            
+            tri_indices.push(idx0, idx2, idx1, idx1, idx2, idx3);
         }
     }
 
-    { 
+    {
         let i = num_segs;
         for (let j = 0; j <= num_segs - 1; j++) {
             let idx0 = i * (num_segs + 1) + j;
             let idx1 = idx0 + 1;
 
-            inidices.push(idx0, idx1);
-        }                
+            indices.push(idx0, idx1);
+        }
     }
 
     {
@@ -47,12 +64,12 @@ let init_grid = (num_segs: number) => {
             let idx0 = i * (num_segs + 1) + j;
             let idx1 = (i + 1) * (num_segs + 1) + j;
 
-            inidices.push(idx0, idx1);
-        }                
+            indices.push(idx0, idx1);
+        }
 
     }
-    
-    return {positions, inidices};
+
+    return { positions, indices, texcoords, tri_indices };
 };
 
 export default init_grid;
--- src/Main.ts
+++ src/Main.ts
@@ -26,7 +26,7 @@ cam_rotation_input_v.step = '0.01';
 cam_rotation_input_v.addEventListener('input', handle_cam_rotation_v);
 cam_rotation_input_v.addEventListener('change', handle_cam_rotation_v);
 */
-let cam_dist = 50;
+let cam_dist = 2;
 let cam_rotation_h = 0;
 let cam_rotation_v = 0;
 
@@ -42,11 +42,11 @@ let update_cam_rotation_v = (_:Event, ui:any) =>{
 }
 GL_UI.setupSlider("#cam_rotation_h", {value: cam_rotation_h, slide: update_cam_rotation_h, max: 2 * Math.PI, min: 0, step: 0.001, precision: 3 });
 GL_UI.setupSlider("#cam_rotation_v", {value: cam_rotation_v, slide: update_cam_rotation_v, max: Math.PI / 2, min: 0, step: 0.001, precision: 3 });
-GL_UI.setupSlider("#cam_dist", {value: cam_dist, slide: update_cam_dist, max: 100, min: 10, step: 0.1, precision: 2 });
+GL_UI.setupSlider("#cam_dist", {value: cam_dist, slide: update_cam_dist, max: 100, min: 1, step: 0.1, precision: 2 });
 
 
 
-let grid_info = init_grid(50);
+let grid_info = init_grid(250);
 
 let throwOnGLError = (err: number, funcName: string, _: any) => {
   throw WebGLDebugUtils.glEnumToString(err)
@@ -57,39 +57,74 @@ const canvas: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById('vi
 let gl = canvas.getContext('webgl2');
 gl = WebGLDebugUtils.makeDebugContext(gl, throwOnGLError);
 
-let vs = `
-#version 300 es
-
+let vs = `#version 300 es
+#define M_PI 3.1415926535897932384626433832795
+#pragma vscode_glsllint_stage : vert
 in vec4 position;
+in vec2 texcoord;
 
 uniform mat4 perspective;
 uniform mat4 camera;
 uniform mat4 scale;
-uniform float A;
-uniform vec2 D;
 
-uniform float w;
-uniform float S;
-uniform float t;
+uniform float A[3];
+uniform float L[3];
+uniform vec2 D[3];
+uniform float w[3];
+uniform float S[3];
+uniform float t; 
+
+out vec3 frag_pos;
+out vec2 _tex;
 
 void main() {
-  vec4 world =  scale * position;
-  float height = A * sin(dot(D, world.xz) * w + t * S);
-  float newX = world.x + 0.2 * A * D.x * cos(w * dot(D, world.xz) + t*S) / (w * A);
-  float newZ = world.z + 0.2 * A * D.y * cos(w * dot(D, world.xz) + t*S) / (w * A);
-  world.x = newX;
-  world.z = newZ;
-  world.y = height;
+  float height = 0.0;
+  for(int i = 0; i < 3; i++) {
+    vec2 normD = normalize(D[i]);
+    height += A[i] * sin((dot(normD, texcoord) * w[i] + t*S[i]));    
+    
+  }
+
+  _tex = texcoord;
+  
+  vec4 world =  scale * vec4(position.x, height, position.z, 1.0);
+  //world.y = height;
+
+  frag_pos = (world / world.w).xyz;
   gl_Position = perspective * camera * world;
+
 }`;
-let fs = `
-#version 300 es
-precision mediump float;
+let fs = `#version 300 es
+precision highp float;
+#pragma vscode_glsllint_stage : frag
+
+in vec2 _tex;
+in vec3 frag_pos;
+
+uniform float A[3];
+uniform float L[3];
+uniform vec2 D[3];
+uniform float w[3];
+uniform float S[3];
+uniform float t;
 
 out vec4 fragColor;
 
 void main() {
-  fragColor = vec4(0, 1, 1, 1);
+  float driX = 0.0;
+  float driY = 0.0;
+  vec3 normal;
+  for(int i = 0; i < 3; i++) {
+    vec2 normD = normalize(D[i]);
+    driX += normD.x * w[i] * A[i] * cos(dot(normD, _tex)*w[i] + t*S[i]);
+    driY += normD.y * w[i] * A[i] * cos(dot(normD, _tex)*w[i] + t*S[i]);    
+  }
+  normal = normalize(vec3(-driX, 1, -driY));
+  vec3 light_pos = vec3(0, 100, 0);
+  vec3 light_dir = normalize(light_pos);
+  float intensity = dot(light_dir, normal);
+  vec3 normal_color = (normal + 1.0) /2.0;
+  fragColor = vec4(0, intensity, 0, 1);
 }`;
 
 const programInfo = twgl.createProgramInfo(gl, [vs, fs]);
@@ -97,12 +132,19 @@ const programInfo = twgl.createProgramInfo(gl, [vs, fs]);
 gl.enable(gl.DEPTH_TEST);
 
 let positions = grid_info.positions;
-let indices = grid_info.inidices;
+let indices = grid_info.tri_indices;
+let texcoords =grid_info.texcoords;
 
-const arrays = {
+console.log(texcoords.length);
+console.log(positions.length);
+const arrays = {  
   position: {
     numComponents: 3,
     data: positions,
+  },
+  texcoord: {
+    numComponents: 2,
+    data: texcoords
   },
   indices: {
     numComponents: 2,
@@ -120,24 +162,28 @@ function render(time: number) {
   let perspective = twgl.m4.perspective(45 / 180 * Math.PI, canvas.clientWidth / canvas.clientHeight, 0.01, 1000);
   let camera = twgl.m4.inverse(twgl.m4.lookAt([cam_dist * Math.sin(cam_rotation_h) * Math.cos(cam_rotation_v), cam_dist * Math.sin(cam_rotation_v), cam_dist * Math.cos(cam_rotation_h) * Math.cos(cam_rotation_v)], twgl.v3.create(0, 0, 0), twgl.v3.create(0, 1, 0)));
   let scale = twgl.m4.scale(twgl.m4.identity(), [10, 10, 10]);
+  let wave_l = [10 / 250, 30 / 250, 15 / 250];
+  let v = 10 / 250;
+  let k_amp_len = 0.1;
 
-  
   const uniforms = {
     t: time * 0.001,
+    t_: [0, 3, 6, 9],
     resolution: [gl.canvas.width, gl.canvas.height],
     perspective: perspective,
     camera: camera,
     scale: scale,
-    D: [1, 1],
-    A: 2,
-    w: 2 / 2,
-    S: 3
+    D: [1, 1, 1, 0.8, 1, 1.2],
+    A: wave_l.map(x=>x * k_amp_len),
+    L: wave_l,
+    w: wave_l.map(x=>2 * Math.PI / x),
+    S: wave_l.map(x=>v * Math.PI * 2 / x)
   };
 
   gl.useProgram(programInfo.program);
   twgl.setBuffersAndAttributes(gl, programInfo, bufferInfo);
   twgl.setUniforms(programInfo, uniforms);
-  twgl.drawBufferInfo(gl, bufferInfo, gl.LINES);
+  twgl.drawBufferInfo(gl, bufferInfo, gl.TRIANGLES);
 
 
   requestAnimationFrame(render);
